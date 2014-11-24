06dfba8438251807306fe441c6c82774320e27a3
Carlos <carlos@carlos-PC.(none)>
2014-11-24 10:02:58 -0430

    Acomodando leccion1-1.html Borrando leccion1-[2-9].html

---
+++ evaluate.js
@@ -0,0 +1,6 @@
+function change(){
+    if(document.getElementById("evaluacion").getAttribute("class")=="glyphicon glyphicon-remove")
+        document.getElementById("evaluacion").setAttribute("class","glyphicon glyphicon-ok green")
+    else
+        document.getElementById("evaluacion").setAttribute("class","glyphicon glyphicon-ok green")
+}
